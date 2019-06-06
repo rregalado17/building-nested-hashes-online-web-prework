de1de02d3e4aa186c2498e3cd07d47e960f7e31e
magical-flow-3186 <rregalado17@gmail.com>
2019-06-06 19:08:09 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,22 +5,13 @@ def third_challenge
         name: "Lord Montague", age: "53"
         },
       :matriarch => {
-        name: "Lady Mantague", age: "54"
+        name: "Lady Montague", age: "54"
         },
       :hero => {
         name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => [
-        {
-          :name => "Benvolio",
-          :age => "17",
-          :attitude => "worried"
-        },
-        {
-          :name => "Mercutio",
-          :age => "18",
-          :attitude => "hot-head"
-        }
+
       ]
    }, 
    :capulet => {
@@ -31,19 +22,10 @@ def third_challenge
         name: "Lady Capulet", age: "51"
         },
       :heroine => {
-        name: "Juliet", age: "15", statue: "alive"
+        name: "Juliet", age: "15", status: "alive"
         },
       :heroine_friends => [
-        {
-          :name => "Steven",
-          :age => "30",
-          :attitude => "confused"
-        },
-        {
-          :name => "Nurse",
-          :age => "44",
-          :attitude => "worried"
-        }
+
         ]
    }
   }
