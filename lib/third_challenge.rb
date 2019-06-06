2bcae26ebdd574ff0ca0718479b173d4d7d39def
magical-flow-3186 <rregalado17@gmail.com>
2019-06-06 18:39:46 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,13 +2,13 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name: ""
+        name: "Lord Montague", age: "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Mantague", age: "54"
         },
       :hero => {
-        #your key/value pairs here
+        name: "Romeo", age: "15"
         },
       :hero_friends => []
    }, 
