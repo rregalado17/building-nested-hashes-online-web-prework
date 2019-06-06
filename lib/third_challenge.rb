b840068e8b39d972671c760f1afa773bc2cf5290
magical-flow-3186 <rregalado17@gmail.com>
2019-06-06 19:03:12 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,19 +10,41 @@ def third_challenge
       :hero => {
         name: "Romeo", age: "15"
         },
-      :hero_friends => []
+      :hero_friends => [
+        {
+          :name => "Benvolio",
+          :age => "17",
+          :attitude => "worried"
+        },
+        {
+          :name => "Mercutio",
+          :age => "18",
+          :attitude => "hot-head"
+        }
+      }]
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        name: "Lord Capulet", age: "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Capulet", age: "51"
         },
       :heroine => {
-        #your key/value pairs here
+        name: "Juliet", age: "15"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {
+          :name => "Steven",
+          :age => "30",
+          :attitude => "confused"
+        },
+        {
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
+        }
+        ]
    }
   }
 
